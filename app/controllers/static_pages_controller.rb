bb4bf11734fc6eb320e9ec02247e5a3e8714efe4
azufb <curiousazunas@gmail.com>
2023-04-08 18:27:07 +0900

    説明コメント追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
+# generateスクリプト実行時に、StaticPagesのようにキャメルケースで渡すことで、
+# コントローラ名がスネークケースになったコントローラファイルが自動生成される。
+# 単なる慣習であり、generateスクリプト実行時にスネークケースで渡しても問題はない。
+# rails generateは関連するファイルを自動生成するコマンド
 class StaticPagesController < ApplicationController
+  # Rubyでは、メソッド(アクション)の中身が空であれば、何も実行しないが、
+  # Railsの場合は、ApplicationControllerを継承しており、対応するビューが表示される。
   def home
   end
 
