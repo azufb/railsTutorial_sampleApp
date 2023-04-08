8e8dec31aa0e3de94a5e3244980b3d94c233fc2d
azufb <curiousazunas@gmail.com>
2023-04-08 18:44:25 +0900

    testコード書いてみる

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,24 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # GETリクエストをhomeアクションに対して送信
+  # assertionで確認(ここでは、レスポンスが200/successになる)
   test "should get home" do
     get static_pages_home_url
     assert_response :success
   end
 
+  # GETリクエストをhelpアクションに対して送信
+  # assertionで確認(ここでは、レスポンスが200/successになる)
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  # GETリクエストをaboutアクションに対して送信
+  # assertionで確認(ここでは、レスポンスが200/successになる)
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 end
