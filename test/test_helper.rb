253d59d8438fbf345e2fa65c3a9fd4df1742a771
azufb <curiousazunas@gmail.com>
2023-04-08 19:35:26 +0900

    minitest reportersの導入

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+# minitest reportersは、テスト結果を見やすく出力するgem
+# 色で成功/失敗を表現したり、パーセンテージなど出力してくれる
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
