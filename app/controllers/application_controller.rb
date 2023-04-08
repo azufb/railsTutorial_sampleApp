d86978ff7cded93ca837fffa6f09874ede044e30
azufb <curiousazunas@gmail.com>
2023-04-08 17:54:48 +0900

    初期表示の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    def greeting
-        render html: 'Hello world!'
-    end
+    def hello
+        render html: "hello, world!"
+      end
 end
