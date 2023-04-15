16c94f25fd1573b7a2b5a97f5279519f658f0ff0
azufb <curiousazunas@gmail.com>
2023-04-15 18:43:21 +0900

    コメント追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   # 名前付きルーティングを定義
+  # asオプションで変更も可能
   get  "/help",    to: "static_pages#help"
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
