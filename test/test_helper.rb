e449640558c811fc5cc84188d12a24ab0787e590
azufb <curiousazunas@gmail.com>
2023-04-15 19:18:41 +0900

    testでApplicationHelperを使えるようにした

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,8 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # includeして、test環境でもApplicationヘルパーを使えるようにする
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
