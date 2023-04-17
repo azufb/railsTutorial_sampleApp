0ef35b0c9232eb1ac10ca457b3a47952d6f910ea
azufb <curiousazunas@gmail.com>
2023-04-17 22:15:17 +0900

    メールアドレスを一意とする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+    # データベースに保存される直前にemailを小文字に変換する
+    # before_save { self.email = email.downcase }
+    before_save { email.downcase! }
+
     # validates(:name, presence: true)の書き方でもOK
     validates :name, presence: true, length: { maximum: 50 }
     # 有効なメールアドレスの正規表現(大文字で始まる名前は定数)
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
