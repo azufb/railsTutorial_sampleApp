45a9a529a9866f3540fc03e7dfaedb02493a5f37
azufb <curiousazunas@gmail.com>
2023-04-18 22:27:05 +0900

    パスワードのハッシュ化等

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
     # 有効なメールアドレスの正規表現(大文字で始まる名前は定数)
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+
+    # ハッシュ化したパスワードをデータベース内のpassword_digest属性に保存できる
+    # passwordとpassword_confirmationが使えるようになり、存在と値が一致するかどうかのバリデーションも追加
+    # authenticateメソッドが使えるようになる
+    has_secure_password
+
+    validates :password, presence: true, length: { minimum: 6 }
 end
