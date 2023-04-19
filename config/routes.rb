27467b8b47edb9107c15b645d8ff51cba46979d8
azufb <curiousazunas@gmail.com>
2023-04-19 22:22:05 +0900

    登録されているユーザ表示

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
+
+  # 名前付きルーティング・RESTfulなUsersリソースで必要な全てのアクションが利用できるようになる
+  resources :users
 end
