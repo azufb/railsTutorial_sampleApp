3c6379440be3f02c0b3d9a83b234c7185ceaa5bb
azufb <curiousazunas@gmail.com>
2023-05-02 15:50:40 +0900

    アカウント有効化

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,23 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      # セッション固定攻撃に対応するため、ログイン直前にセッションをリセットする
-      # 新しいセッションidが使われるようになる
-      reset_session
-      # フォームで送られてきたremember_meの値によって、処理分岐
-      # 値が1であれば(チェックが入っていれば)、記憶する
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      # sessions_helperで定義したlog_in関数でログイン
-      log_in user
-      redirect_to forwarding_url || user
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        # セッション固定攻撃に対応するため、ログイン直前にセッションをリセットする
+        # 新しいセッションidが使われるようになる
+        reset_session
+        # フォームで送られてきたremember_meの値によって、処理分岐
+        # 値が1であれば(チェックが入っていれば)、記憶する
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        # sessions_helperで定義したlog_in関数でログイン
+        log_in user
+        redirect_to forwarding_url || user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
