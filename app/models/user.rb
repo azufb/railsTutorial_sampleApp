1d76bd990f5a453d905ffa105f07928e05f0c3cd
azufb <curiousazunas@gmail.com>
2023-05-03 18:01:19 +0900

    PasswordRestsリソース

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     # インスタンス変数remember_tokenに対する読み取りメソッドと書き込みメソッドの両方を定義
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     # データベースに保存される直前にemailを小文字に変換する
     # before_save :メソッド名とすることで、メソッドを探して実行してくれる（メソッド参照）
     before_save :downcase_email
@@ -67,6 +67,18 @@ class User < ApplicationRecord
     # 有効化用のメールを送信する
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+
+    # パスワード再設定の属性を設定する
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest,  User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # パスワード再設定のメールを送信する
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
     end
 
     private
